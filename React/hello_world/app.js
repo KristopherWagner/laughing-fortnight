095678025b834dc0c7468d921139e5fbb7ef520d
KristopherWagner <kwagner@novetta.com>
2017-03-06 13:09:57 +0000

    Use a button

--- React/hello_world/app.js
+++ React/hello_world/app.js
@@ -1,9 +1,21 @@
 var HelloWorld = React.createClass({
-    render: function() {
-        return <div>Hello, {this.props.name}!</div>;
-    }
+  getInitialState: function() {
+    return {
+      greeting: ""
+    };
+  },
+
+  greet: function() {
+    this.setState({ greeting: "Hello, " + this.props.name + "!" });
+  }, 
+
+  render: function() {
+    return <div>
+      <button onClick={this.greet}>Greet</button>
+      <br />
+      <span>{this.state.greeting}</span>
+      </div>;
+  }
 });
 
-React.render(new HelloWorld({
-    name: "World"
-}), document.body);
+React.render(new HelloWorld({ name: "World" }), document.body);
