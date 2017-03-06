ddd14527a09712603c22f666f906aeb1060510bd
KristopherWagner <kwagner@novetta.com>
2017-03-06 12:43:39 +0000

    Beautify

--- Angular/hello_world/app.js
+++ Angular/hello_world/app.js
@@ -3,8 +3,8 @@ app.controller("testController", function() {
 });
 
 app.directive("helloWorld", function() {
-  return {
-    restrict: "E",
-    template: "<div>Hello, world!</div>"
-  };
+    return {
+        restrict: "E",
+        template: "<div>Hello, world!</div>"
+    };
 });
