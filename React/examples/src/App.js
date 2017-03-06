38c1b585ebe7315cd481549016bfb21b66c1daa8
KristopherWagner <kwagner@novetta.com>
2017-03-06 16:30:14 +0000

    Add a clock

--- React/examples/src/App.js
+++ React/examples/src/App.js
@@ -2,17 +2,50 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+        () => this.tick(), 
+        1000
+        );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+        <div>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+        );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Simple React Examples</h2>
         </div>
         <p className="App-intro">
           Hello, World!
         </p>
+        <Clock />
       </div>
     );
   }
