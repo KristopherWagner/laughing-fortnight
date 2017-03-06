d396f05775132540fda116200484c5254cc0fe5a
KristopherWagner <kwagner@novetta.com>
2017-03-06 14:39:48 +0000

    Beautify

--- Angular/hello_world/app.js
+++ Angular/hello_world/app.js
@@ -1,32 +1,32 @@
 var app = angular.module("testApp", {});
 
-app.controller("testController", function ($scope)) {
-  $scope.name = "World";
+app.controller("testController", function($scope)) {
+$scope.name = "World";
 });
 
-app.directive("helloWorld", function () {
-  return {
-    restrict: "E",
-    template: document.getElementById("hello-world-template").innerHTML,
-    scope: {
-      name: "@"
-    },
-    link: function(scope) {
-      scope.greetings = [];
+app.directive("helloWorld", function() {
+    return {
+        restrict: "E",
+        template: document.getElementById("hello-world-template").innerHTML,
+        scope: {
+            name: "@"
+        },
+        link: function(scope) {
+            scope.greetings = [];
 
-      scope.greet = function() {
-        scope.greetings.push("Hello, " + scope.name + "!");
-        scope.greetings.push("Hi, " + scope.name + "!");
-        scope.greetings.push("Hey, " + scope.name + "!");
-      }
-    }
-  };
+            scope.greet = function() {
+                scope.greetings.push("Hello, " + scope.name + "!");
+                scope.greetings.push("Hi, " + scope.name + "!");
+                scope.greetings.push("Hey, " + scope.name + "!");
+            }
+        }
+    };
 });
 
 app.directive("wrapper", function() {
-  return {
-    restrict: "E",
-    template: document.getElementById("wrapper-template").innerHTML,
-    transclude: true
-  }
+    return {
+        restrict: "E",
+        template: document.getElementById("wrapper-template").innerHTML,
+        transclude: true
+    }
 });
