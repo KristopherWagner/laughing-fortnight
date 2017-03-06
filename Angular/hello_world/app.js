4381e2fbd51f6071d15bfd62085d8a5fd170e92e
KristopherWagner <kwagner@novetta.com>
2017-03-06 12:50:11 +0000

    Use scope

--- Angular/hello_world/app.js
+++ Angular/hello_world/app.js
@@ -1,10 +1,15 @@
-app.controller("testController", function() {
+var app = angular.module("testApp", {});
 
+app.controller("testController", function($scope)) {
+$scope.name = "World";
 });
 
 app.directive("helloWorld", function() {
     return {
         restrict: "E",
-        template: "<div>Hello, world!</div>"
+        template: "<div>Hello, {{name}!</div>",
+        scope: {
+            name: "@"
+        }
     };
 });
